8e349f41b4cf02dfdb790736e0207a618768b474
MohamedAmer29 <122878764+MohamedAmer29@users.noreply.github.com>
2023-02-16 13:16:56 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,6 @@
  */
 int main (void)
 {
-  printf("\"Programming is like building a multilingual puzzle");
-
-  return 0;
+      printf("\"with proper grammar, but the outcome is a piece of art,")
+      return 0;
 }
